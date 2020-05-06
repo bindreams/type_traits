08dc2d963fe967261c321e4ef16d1d695bf41f1d
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2020-05-06 19:07:24 +0300

    Add cmake and conan switches for building tests

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,42 @@
 """Conan package manager config file. https://docs.conan.io/en/latest/"""
-from conans import ConanFile, CMake
+import re
+from conans import ConanFile, CMake, tools
 
-        
+
+def get_project_version():
+    try:
+        content = tools.load("CMakeLists.txt")
+        version = re.search(r"project\([^\)]+VERSION (\d+\.\d+\.\d+)[^\)]*\)", content).group(1)
+        return version.strip()
+    except Exception:
+        return None
+
+		
+def get_project_name():
+    try:
+        content = tools.load("CMakeLists.txt")
+        name = re.search(r"project\(([^\) ]+)[^\)]*\)", content).group(1)
+        return name.strip()
+    except Exception:
+        return None
+
+
 class Project(ConanFile):
     """Conan package file."""
+    name = get_project_name()
+    version = get_project_version()
+
     settings = "os", "compiler", "build_type", "arch"
     requires = ["doctest/2.3.7"]
 
+    options = {
+        "tests": [True, False],
+        "examples": [True, False],
+    }
+
     default_options = {
+        "tests": False,
+        "examples": False,
         "gtest:shared": False
     }
 
@@ -27,7 +56,11 @@ class Project(ConanFile):
     def build(self):
         cmake = CMake(self, generator="Ninja")
         cmake.definitions["CMAKE_EXPORT_COMPILE_COMMANDS"] = True
+
+        if self.options.tests:
+            cmake.definitions[self.name + "_BUILD_TESTS"] = True
+        if self.options.examples:
+            cmake.definitions[self.name + "_BUILD_EXAMPLES"] = True
+
         cmake.configure()
         cmake.build()
-
-        self.run("ctest -VV -C %s" % cmake.build_type, run_environment=True)
